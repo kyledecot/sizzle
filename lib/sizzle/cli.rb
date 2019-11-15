7fc80183faa9a93d40fc013b89327056f68d874b
Kyle Decot <kyle@joinroot.com>
2019-11-15 18:25:44 -0500

    Remove All But Device

--- lib/sizzle/cli.rb
+++ lib/sizzle/cli.rb
@@ -3,29 +3,12 @@ require "colorize"
 require "terminal-table"
 
 require "sizzle/ios/device"
-require "sizzle/ios/build"
-require "sizzle/ios/app"
 
 class Sizzle 
   class CLI 
     extend GLI::App
     
     command :ios do |ios|
-      ios.flag %i(app-store-connect-key-id)
-      ios.flag %i(app-store-connect-issuer-id)
-      ios.flag %i(app-store-connect-private-key)
-
-      ios.command :builds do |builds|
-        builds.command :list do |list|
-          list.action do |global, options, args|
-            puts Terminal::Table.new(
-              title: "Builds".green,
-              rows: IOS::Build.list(global.merge(options)).map { |b| b[:attributes].values }
-            )          
-          end 
-        end 
-      end 
-
       ios.command :devices do |devices|
         devices.command :create do |create|
           create.flag :name
@@ -45,22 +28,14 @@ class Sizzle
           list.flag :limit, :type => Integer, :default_value => 100
 
           list.action do |global, options, args|
+            devices = IOS::Device.list(limit: options[:limit])
+
+
             puts Terminal::Table.new(
               :title => 'Devices'.green,
-              :rows => IOS::Device.list(limit: options[:limit]).map { |d| d[:attributes].values }
+              :rows => devices.map { |d| d.to_h.values }
             )
           end 
-        end 
-      end 
-
-      ios.command :apps do |apps|
-        apps.action do 
-          table = Terminal::Table.new(
-            :title => 'Apps',
-            :rows => IOS::App.list.map { |d| d[:attributes].values }
-          )
-
-          puts table
         end 
       end 
     end 
--- lib/sizzle/ios/device.rb
+++ lib/sizzle/ios/device.rb
@@ -3,12 +3,28 @@ require "sizzle/ios/client"
 class Sizzle 
   class IOS
     class Device
+      ATTRIBUTES = %i[id udid device_class model name platform status added_date].freeze
+
+      attr_reader *ATTRIBUTES
+
+      def initialize(options = {})
+        ATTRIBUTES.each do |attribute|
+          instance_variable_set("@#{attribute}", options[attribute])
+        end 
+      end 
+      
+      def to_h 
+        Hash[ATTRIBUTES.map { |a| [a, instance_variable_get("@#{a}")] }]
+      end 
+
       def self.create(options = {})
-        CLIENT.create_device(options.slice(:name, :udid, :platform))
+        CLIENT.create_device(options.slice(ATTRIBUTES))
       end 
 
       def self.list(options = {})
-        CLIENT.devices(limit: options[:limit])[:data]
+        CLIENT.devices(limit: options[:limit])[:data].map do |result|
+          new(result[:attributes].merge(id: result[:id]))
+        end 
       end 
     end 
   end 
